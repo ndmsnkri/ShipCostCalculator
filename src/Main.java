a35c29e14c509b9c7c21dca68c65768f36e7b2ab
ndmsnkri <144985362+ndmsnkri@users.noreply.github.com>
2023-09-24 17:28:45 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main
+    {
+    public static void main(String[] args)
+        {
+            Scanner in = new Scanner(System.in);  // create a Scanner in to read from the console
+
+            double itemPrice = 0;
+            String trash = ""; // use for bad input which will read as a String
+            System.out.print("Enter your item price: ");
+
+            if(in.hasNextDouble())
+            {
+                // OK safe to read in a double
+                itemPrice = in.nextDouble();
+                in.nextLine(); // clear the buffer
+                System.out.println("You said your item price was: " + itemPrice + "$");
+            }
+            double shippingCost;
+            if (itemPrice >= 100)
+            {
+                shippingCost = 0; // Shipping is free for items $100 or more
+            }
+            else
+            {
+                shippingCost = 0.02 * itemPrice;
+                // Shipping cost is 2% of the item price
+            }
+
+            // Calculate the total price
+            double totalPrice = itemPrice + shippingCost;
+
+            // Display the shipping cost and total price
+            System.out.printf("Shipping cost: $%.2f%n", shippingCost);
+            System.out.printf("Total price: $%.2f%n", totalPrice);
+
+        }
+    }
